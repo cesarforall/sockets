31e7caa186b740e6bab97078d2e604d424704230
César Almeida <cesaralmeida89@gmail.com>
2025-04-06 21:42:54 +0200

    Implement cliente

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,5 +1,39 @@
 package cliente;
 
-public class Cliente {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Scanner;
 
+public class Cliente {
+	private final String HOST = "localhost";
+	private final int PUERTO = 4321;
+	private Socket socket;
+	
+	public Cliente() throws IOException {
+		socket = new Socket(HOST, PUERTO);
+	}
+	
+	public void iniciarCliente() throws IOException {
+		System.out.println("Se ha iniciado el cliente.");
+		
+		try (Scanner scanner = new Scanner(System.in)) {
+			DataInputStream mensajeServidor = new DataInputStream(socket.getInputStream());
+			DataOutputStream respuesta = new DataOutputStream(socket.getOutputStream());
+			
+			String inputConsola = "";
+			
+			while (true) {			
+				String mensaje = mensajeServidor.readUTF();
+				System.out.println(mensaje);
+				inputConsola = scanner.nextLine();
+				respuesta.writeUTF(inputConsola);
+			}
+		} catch (SocketException e) {
+			System.out.println("El servidor ha cerrado la conexión.");
+		}		
+		
+	}
 }
--- src/cliente/MainCliente.java
+++ src/cliente/MainCliente.java
@@ -1,5 +1,13 @@
 package cliente;
 
+import java.io.IOException;
+
 public class MainCliente {
 
+	public static void main(String[] args) throws IOException {
+		Cliente cliente = new Cliente();		
+		
+		cliente.iniciarCliente();
+	}
+	
 }
